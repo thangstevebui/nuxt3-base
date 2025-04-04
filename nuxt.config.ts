c7220e110d192d868c1ab6139e4c46895ef5fbd2
thangstevebh <thangstevebh@gmail.com>
2025-04-04 09:43:22 +0700

    ✨ Implement chart
    implement chart

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  vue: {
+    propsDestructure: true,
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "shadcn-nuxt",
---
+++ pages/(app)/chart/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="h1">Chart page</div>
+
+    <div id="chart">
+      <svg ref="svg"></svg>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+  name: "Chart",
+  mounted() {
+    this.createChart();
+  },
+  methods: {
+    createChart() {
+      const data = [30, 86, 168, 281, 303, 365];
+      const width = 500;
+      const height = 300;
+
+      const svg = d3
+        .select(this.$refs.svg)
+        .attr("width", width)
+        .attr("height", height);
+
+      svg
+        .selectAll("rect")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => i * 70)
+        .attr("y", (d) => height - d)
+        .attr("width", 65)
+        .attr("height", (d) => d)
+        .attr("fill", "#69b3a2");
+    },
+  },
+};
+</script>
+
+<style>
+svg {
+  border: 1px solid black;
+}
+</style>
